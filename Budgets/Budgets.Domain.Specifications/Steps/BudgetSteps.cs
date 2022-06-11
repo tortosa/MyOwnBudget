63ec99e246b62a5d3f7453046d03b7318cb58a53
Daniel Tortosa Gonzalez <tortosad@ryanair.com>
2022-06-11 19:06:31 +0200

    Added GivenBudget step

---
+++ Budgets/Budgets.Domain.Specifications/Steps/BudgetSteps.cs
@@ -0,0 +1,31 @@
+﻿using Budgets.Domain.Specifications.Contexts;
+using Budgets.Domain.Specifications.Model;
+using Budgets.Domain.Specifications.Steps.Given;
+using System.Linq;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace Budgets.Domain.Specifications.Steps
+{
+    [Binding]
+    public class BudgetSteps
+    {
+        private readonly BudgetContext budgetContext;
+
+        public BudgetSteps(BudgetContext budgetContext)
+        {
+            this.budgetContext = budgetContext;
+        }
+
+        [Given(@"Budgets")]
+        public void GivenBudgets(Table table)
+        {
+            var modelList = table.CreateSet<BudgetModel>().ToList();
+            if (modelList.Select(model => model.Id).Count() != modelList.Count)
+                Assert.True(false, "Check your budget ids, they may be duplicated");
+
+            budgetContext.Budgets = GivenBuilderFactory.GivenBudgets(modelList);
+        }
+    }
+}
---
+++ Budgets/Budgets.Domain.Specifications/Steps/Given/GivenBuilderFactory.cs
@@ -0,0 +1,28 @@
+﻿using Budgets.Domain.Specifications.Model;
+using Budgets.Tests.Common.Builders;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Budgets.Domain.Specifications.Steps.Given
+{
+    public static class GivenBuilderFactory
+    {
+        public static IEnumerable<BudgetBuilder> GivenBudgets(IEnumerable<BudgetModel> budgets)
+        {
+            var domainBudgets = new List<BudgetBuilder>();
+
+            foreach (var budget in budgets)
+            {
+                Assert.False(string.IsNullOrEmpty(budget.Id));
+
+                domainBudgets.Add(
+                    new BudgetBuilder()
+                    .WithLabel(budget.Label)
+                    .WithDateFormat(budget.DateFormat)
+                    .WithCurrencyCode(budget.Currency)
+                    );
+            }
+            return domainBudgets;
+        }
+    }
+}
