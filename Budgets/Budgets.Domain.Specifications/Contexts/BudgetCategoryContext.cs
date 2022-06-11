da5b8b78bb858fad745a2fdb413f6599a3bc74fa
Daniel Tortosa Gonzalez <tortosad@ryanair.com>
2022-06-11 19:05:47 +0200

    Created some contexts

---
+++ Budgets/Budgets.Domain.Specifications/Contexts/BudgetCategoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Budgets.Tests.Common.Builders;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Budgets.Domain.Specifications.Contexts
+{
+    public class BudgetCategoryContext
+    {
+        public BudgetCategoryContext()
+        {
+            BudgetCategories = Enumerable.Empty<BudgetCategoryBuilder>();
+        }
+
+        public IEnumerable<BudgetCategoryBuilder> BudgetCategories { get; set; }
+    }
+}
